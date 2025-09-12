ccfd1360ca37813a57374b525bb47cedc5b71f6c
shukebeta <shukebeta@debian12>
2025-09-13 08:23:09 +1200

    Working examples added

--- sync_files.sh
+++ sync_files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 定义源和目标目录
+# define source and destination dirs
 SOURCE_DIR="/home/shukebeta/Projects/immich-app/library"
 DEST_DIR1="xps:/backup/photos"
 DEST_DIR2="arm:~/backups/photos"
 
-# 执行 rsync 命令
+# do syncing every a few minuts: you'll need a cron task to run this script
 rsync -av --exclude='tmp/' "$SOURCE_DIR" "$DEST_DIR1"
 rsync -av --exclude='tmp/' "$SOURCE_DIR" "$DEST_DIR2"
 
